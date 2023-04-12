f17ddf2b0a71f4708ba9b1407d4556005705a2a5
Lucas-BF <lucasbf4@gmail.com>
2023-04-11 23:28:17 -0300

    Update BonecoImagem.java

--- br/edu/iff/jogoforca/dominio/boneco/imagem/BonecoImagem.java
+++ br/edu/iff/jogoforca/dominio/boneco/imagem/BonecoImagem.java
@@ -1,5 +1,16 @@
 package br.edu.iff.jogoforca.dominio.boneco.imagem;
 
-public class BonecoImagem {
+public class BonecoImagem implements Boneco {
+    private static BonecoImagem soleInstance = new BonecoImagem();
 
+    private BonecoImagem() {}
+
+    public static BonecoImagem getSoleInstance() {
+        return soleInstance;
+    }
+
+    @Override
+    public void exibir(Object contexto, int partes) {
+        // Implementação da exibição do boneco como imagem
+    }
 }
