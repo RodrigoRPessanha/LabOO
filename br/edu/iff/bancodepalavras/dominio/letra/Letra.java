5ec6c77cc6d642665d86ac56343ae59fb535543e
Matheus Oliveira <mofaria9@gmail.com>
2023-04-12 00:33:28 -0300

    Update Letra

--- br/edu/iff/bancodepalavras/dominio/letra/Letra.java
+++ br/edu/iff/bancodepalavras/dominio/letra/Letra.java
@@ -1,5 +1,32 @@
 package br.edu.iff.bancodepalavras.dominio.letra;
 
-public class Letra {
+public abstract class Letra {
+	private char codigo;
 
+	protected Letra(char codigo) {
+		this.codigo = codigo;
+	}
+
+	public char getCodigo() {
+		return this.codigo;
+	}
+
+	public void exibir(Object contexto){
+		System.out.println(contexto);
+	}
+
+	public boolean equals(Object o){
+		if (!(o instanceof Letra)) return false;
+		Letra outra = (Letra) o;
+		return this.codigo == outra.codigo && this.getClass().equals(outra.getClass());
+	}
+
+	public int hashcode() {
+		return this.codigo+this.getClass().hashCode();
+	}
+
+	@Override
+	public final String toString() {
+		return String.valueOf(this.codigo);
+	}
 }
--- br/edu/iff/bancodepalavras/dominio/letra/imagem/LetraImagem.java
+++ br/edu/iff/bancodepalavras/dominio/letra/imagem/LetraImagem.java
@@ -1,5 +1,13 @@
 package br.edu.iff.bancodepalavras.dominio.letra.imagem;
 
-public class LetraImagem {
+public class LetraImagem extends br.edu.iff.bancodepalavras.dominio.letra.Letra{
 
+	public LetraImagem(char codigo) {
+		super(codigo);
+	}
+
+	@Override
+	public void exibir(Object contexto){
+		System.out.println(contexto);
+	}
 }
