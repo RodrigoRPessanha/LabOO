e36cfdce17f7e588108972800da095eff59190e0
Lucas-BF <lucasbf4@gmail.com>
2023-04-11 23:27:26 -0300

    Update BonecoTexto.java

--- br/edu/iff/jogoforca/dominio/boneco/texto/BonecoTexto.java
+++ br/edu/iff/jogoforca/dominio/boneco/texto/BonecoTexto.java
@@ -1,5 +1,17 @@
 package br.edu.iff.jogoforca.dominio.boneco.texto;
 
-public class BonecoTexto {
+public class BonecoTexto implements Boneco {
+    private static BonecoTexto soleInstance = new BonecoTexto();
 
+    private BonecoTexto() {}
+
+    public static BonecoTexto getSoleInstance() {
+        return soleInstance;
+    }
+
+    @Override
+    public void exibir(Object contexto, int partes) {
+        // Implementação da exibição do boneco como texto
+    }
 }
+
