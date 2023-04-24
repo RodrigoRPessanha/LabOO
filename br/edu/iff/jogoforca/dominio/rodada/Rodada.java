ea55958151089f21bf6003dd869710fc0ec52de0
Matheus Oliveira <mofaria9@gmail.com>
2023-04-23 22:03:13 -0300

    Progresso em progresso

--- br/edu/iff/jogoforca/dominio/rodada/Rodada.java
+++ br/edu/iff/jogoforca/dominio/rodada/Rodada.java
@@ -1,6 +1,5 @@
 package br.edu.iff.jogoforca.dominio.rodada;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import br.edu.iff.bancodepalavras.dominio.letra.Letra;
@@ -15,26 +14,104 @@ public class Rodada extends br.edu.iff.dominio.ObjetoDominioImpl{
     private static int maxErros = 10;
     private static int pontosQuandoDescobreTodasAsPalavras = 100;
     private static int pontosPorLetraEncoberta = 15;
+    private static BonecoFactory bonecoFactory;
     private List<Item> itens;
+    private List<Letra> erradas;
+    private List<Palavra> palavras;
     private Jogador jogador;
+    private Boneco boneco;
+    private Tema tema;
     
-
-    private Rodada(long id, List<Item> itens, List<Letra> erradas, Jogador jogador) {
-        super(id);
+    public static int getMaxPalavras() {
+        return maxPalavras;
+    }
+    
+    public static void setMaxPalavras(int maxPalavras) {
+        Rodada.maxPalavras = maxPalavras;
+    }
+    
+    public static int getMaxErros() {
+        return maxErros;
+    }
+    
+    public static void setMaxErros(int maxErros) {
+        Rodada.maxErros = maxErros;
+    }
+    
+    public static int getPontosQuandoDescobreTodasAsPalavras() {
+        return pontosQuandoDescobreTodasAsPalavras;
+    }
+    
+    public static void setPontosQuandoDescobreTodasAsPalavras(int pontosQuandoDescobreTodasAsPalavras) {
+        Rodada.pontosQuandoDescobreTodasAsPalavras = pontosQuandoDescobreTodasAsPalavras;
+    }
+    
+    public static int getPontosPorLetraEncoberta() {
+        return pontosPorLetraEncoberta;
+    }
+    
+    public static void setPontosPorLetraEncoberta(int pontosPorLetraEncoberta) {
+        Rodada.pontosPorLetraEncoberta = pontosPorLetraEncoberta;
+    }
+    
+    public static void setBonecoFactory(BonecoFactory bonecoFactory) {
+        Rodada.bonecoFactory = bonecoFactory;
     }
 
-    public Rodada(long id, List<Palavra>palavras, Jogador jogador) {
-        super(id);
+    public static BonecoFactory getBonecoFactory() {
+        return bonecoFactory;
     }
 
+    public static Rodada criar(long id, List<Palavra> palavras, Jogador jogador) {
+        Rodada rodada = new Rodada(id, palavras, jogador);
+        return rodada;
+    }
+    
     public static Rodada reconstituir(long id, List<Item> itens, List<Letra> erradas, Jogador jogador) {
         Rodada rodada = new Rodada(id, itens, erradas, jogador);
         return rodada;
     }
     
-    public static Rodada criar(long id, List<Palavra> palavras, Jogador jogador) {
-        Rodada rodada = new Rodada(id, palavras, jogador);
-        return rodada;
+    public Rodada(long id, List<Palavra>palavras, Jogador jogador) { //Tem que terminar
+        super(id);
+        this.palavras = palavras;
+        this.jogador = jogador;
+    }
+
+    private Rodada(long id, List<Item> itens, List<Letra> erradas, Jogador jogador) { //Tem que terminar
+        super(id);
+        this.itens = itens;
+        this.erradas = erradas;
+        this.jogador = jogador;
+    }
+
+    public Jogador getJogador() {
+        return this.jogador;
+    }
+
+    public Tema getTema() {
+        return tema;
+    }
+
+    public List<Palavra> getPalavras() {
+        return palavras;
+    }
+
+    public int getNumPalavras() {
+        return palavras.size();
+    }
+
+    public void tentar(char codigo) { //Tem que terminar
+        if (encerrou()) {
+            return;
+        }
+    }
+
+    public void arriscar(List<String> codigo) {
+        if (encerrou()) {
+			return;
+		}
+		
     }
     
     public void exibirItens(Object contexto) {
@@ -43,6 +120,21 @@ public class Rodada extends br.edu.iff.dominio.ObjetoDominioImpl{
             System.out.println();
         }
     }
+
+    public boolean encerrou(){
+        if (arriscou() || descobriu() || qtdeErros() == maxErros) {
+            return true;
+        }
+        return false;
+    }
+
+	public boolean descobriu() {
+		for (Palavra p : this.palavras) {
+			if (!p.estaDescoberta()) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
 }
-  
-    
--- br/edu/iff/jogoforca/dominio/rodada/RodadaAppService.java
+++ br/edu/iff/jogoforca/dominio/rodada/RodadaAppService.java
@@ -1,5 +1,49 @@
 package br.edu.iff.jogoforca.dominio.rodada;
 
+import br.edu.iff.jogoforca.dominio.jogador.Jogador;
+import br.edu.iff.jogoforca.dominio.jogador.JogadorRepository;
+
 public class RodadaAppService {
 
+    private static RodadaAppService soleInstance;
+    private RodadaFactory rodadaFactory;
+    private RodadaRepository rodadaRepository;
+    private JogadorRepository jogadorRepository;
+
+    private RodadaAppService(RodadaFactory rodadaFactory, RodadaRepository rodadaRepository, JogadorRepository jogadorRepository) {
+        this.rodadaFactory = rodadaFactory;
+        this.rodadaRepository = rodadaRepository;
+        this.jogadorRepository = jogadorRepository;
+    }
+
+    public static RodadaAppService getSoleInstance() {
+        return soleInstance;
+    }
+
+    public static void createSoleInstance(RodadaFactory rodadaFactory, RodadaRepository rodadaRepository, JogadorRepository jogadorRepository) {
+        if (soleInstance == null) {
+            soleInstance = new RodadaAppService(rodadaFactory, rodadaRepository, jogadorRepository);
+        }
+    }
+
+    public boolean salvarRodada(Rodada rodada) {
+        return rodadaRepository.salvar(rodada);
+    }
+
+    public Rodada novaRodada(Jogador jogador) {
+        return rodadaFactory.criar(jogador);
+    }
+
+    public Rodada novaRodada(String nomeJogador) throws JogadorNaoEncontradoException {
+        Jogador jogador = jogadorRepository.buscarPorNome(nomeJogador);
+        if (jogador == null) {
+            throw new JogadorNaoEncontradoException(nomeJogador);
+        }
+        return rodadaFactory.criar(jogador);
+    }
+
+    public Rodada novaRodada(long idJogador) {
+        Jogador jogador = jogadorRepository.buscarPorId(idJogador);
+        return rodadaFactory.criar(jogador);
+    }
 }
